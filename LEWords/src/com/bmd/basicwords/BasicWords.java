a0c67355018d7752f15647eeab41e1f560bcf4cf
Arvin Yan <fkwy0516@163.com>
2019-07-11 02:19:27 +0800

    Create BasicWords.java
    Basic Num.00002

---
+++ LEWords/src/com/bmd/basicwords/BasicWords.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.bmd.basicwords;
+
+/**
+ * @author Arvin
+ *
+ */
+public class BasicWords {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
